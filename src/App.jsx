e22378c64ac6b49bfcce215a56bcf1713ff63254
rmatej22 <rmatej545@gmail.com>
2024-07-01 20:33:20 +0200

    move form state to parent component so it can be shared with other child components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import Header from "./components/Header";
+import Results from "./components/Results";
 import UserInput from "./components/UserInput";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    intialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIndetifier, newValue) {
+    setUserInput((state) => {
+      return {
+        ...state,
+        [inputIndetifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />;
-      <UserInput />
+      <UserInput userInput={userInput} onChangeInput={handleChange} />
+      <Results userInput={userInput} />
     </>
   );
 }
